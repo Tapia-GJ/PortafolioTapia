42466cf8de6f1bc2f020ccd235feee79903a4210
Luis Tapia <133921246+Tapia-GJ@users.noreply.github.com>
2026-03-29 19:16:18 -0500

    add categorias a los proyectos y un filtro por categoria

--- src/types/project.ts
+++ src/types/project.ts
@@ -1,9 +1,12 @@
+export type CategoriaProyecto = "personal" | "profesional";
+
 export interface Proyecto {
   id: string;
   nombreProyecto: string;
   descripcion: string;
   img: string;
   stack: string[];
+  categoria: CategoriaProyecto;
   linkDetalle: string;
   hasLink?: boolean;
   linkSitioWeb?: string;
